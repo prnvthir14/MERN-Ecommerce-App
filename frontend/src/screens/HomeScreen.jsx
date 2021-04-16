7c0307f62b22ef3d907b094cbc644b62c1184f0d
pranav <prnvthir@gmail.com>
2021-04-15 22:53:00 -0400

    add grid layout for products list

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import "./HomeScreen.css";
 
 const HomeScreen = () => {
-  return <div className="homescreen">
-    Home Screen
-  </div>;
+  return (
+    <div className="homescreen"> homescreen
+      <h2 className="homescreen__title">Products</h2>
+      
+      <div className="homescreen__products">
+        {/*products list here  */}
+      
+      </div>
+    </div>
+  );
 };
 
 export default HomeScreen;
