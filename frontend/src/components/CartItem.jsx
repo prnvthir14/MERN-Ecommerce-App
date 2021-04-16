e29a61cb601880295f99ea98abb0d22e521588bb
pranav <prnvthir@gmail.com>
2021-04-16 18:21:36 -0400

    complete cart screen layout, building cartitem component before continuing but working on this branch for simplicity

---
+++ frontend/src/components/CartItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./CartItem.css";
+import { Link } from "react-router-dom";
+
+const CartItem = ({ show, click }) => {
+  return <div className="cartitem">
+    CART COMPONENT
+  </div>;
+};
+
+export default CartItem;
--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-import './CartScreen.css'
+import "./CartScreen.css";
+
+//components
+import CartItem from "../components/CartItem";
 
 const CartScreen = () => {
-  return <div className="cartscreen">
-    CART SCREEN
-  </div>;
+  return (
+    <div className="cartscreen">
+      <div className="cartscreen__left">
+        <h2>Shopping Cart</h2>
+        <CartItem />
+      </div>
+
+      <div className="cartscreen__right">
+        <div className="cartscreen__info"></div>
+        <p>Subtotal (0) items</p>
+        <p>499.99</p>
+      </div>
+      <div>
+        <button>Proceed to Checkout</button>
+      </div>
+    </div>
+  );
 };
 
 export default CartScreen;
