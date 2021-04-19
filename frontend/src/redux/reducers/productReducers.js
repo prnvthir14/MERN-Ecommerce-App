7c3e73489cddc0c5f1feab866832b68b7781db58
pranav <prnvthir@gmail.com>
2021-04-18 22:14:54 -0400

    add productReducers

---
+++ frontend/src/redux/reducers/productReducers.js
@@ -0,0 +1,56 @@
+import * as actionTypes from "../constants/productConstants";
+
+export const getProductsReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case actionTypes.GET_PRODUCTS_REQUESTS:
+      return {
+        loading: true,
+        products: [],
+      };
+
+    case actionTypes.GET_PRODUCTS_SUCCESS:
+      return {
+        loading: false,
+        products: action.payload,
+      };
+
+    case actionTypes.GET_PRODUCTS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+//see individual product details; when some clicks the link to see the individual product
+export const getProdctDetails = (state = { product: {} }, action) => {
+  switch (action.type) {
+    case actionTypes.GET_PRODUCT_DETAILS_REQUESTS:
+      return {
+        loading: true,
+      };
+
+    case actionTypes.GET_PRODUCT_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        product: action.payload,
+      };
+
+    case actionTypes.GET_PRODUCTS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+
+    case actionTypes.GET_PRODUCT_DETAILS_RESET:
+      return {
+        product: {},
+      };
+
+    default:
+      return state;
+  }
+};
