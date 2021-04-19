9abf8f99231b67a2acd44c4578a10d65cb8effad
pranav <prnvthir@gmail.com>
2021-04-18 23:26:49 -0400

    add product Actions

---
+++ frontend/src/redux/actions/productActions.js
@@ -0,0 +1,48 @@
+import * as actionTypes from "../constants/productConstants";
+import axios from "axios";
+
+export const getProducts = () => async (dispatch) => {
+  try {
+    dispatch({
+      types: actionTypes.GET_PRODUCTS_REQUESTS,
+    });
+
+    const { data } = await axios.get("/api/products");
+
+    dispatch({
+      types: actionTypes.GET_PRODUCTS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.GET_PRODUCTS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const getProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      types: actionTypes.GET_PRODUCT_DETAILS_REQUESTS,
+    });
+
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({
+      types: actionTypes.GET_PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.GET_PRODUCTS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
