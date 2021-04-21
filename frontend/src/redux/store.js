98e37cf7f6722b118efb3cfa2308ea657c647783
pranav <prnvthir@gmail.com>
2021-04-21 17:25:59 -0400

    add inital state in store.js to persists cart state on refresh

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -14,14 +14,26 @@ import {
 const reducer = combineReducers({
   cart: cartReducer,
   getProducts: getProductsReducer,
-  getProductDetails: getProductDetailsReducer
+  getProductDetails: getProductDetailsReducer,
 });
 
 //thunk - make asynch requests with redux
 const middleware = [thunk];
 
+//check whats in local storage; if somthing exists, set cartFromLocalStorage to that, otherwise, set to empty arrray
+const cartFromLocalStorage = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : [];
+
+const INITIAL_STATE = {
+  cart: {
+    cartItems: cartFromLocalStorage,
+  },
+};
+
 const store = createStore(
   reducer,
+  INITIAL_STATE,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
