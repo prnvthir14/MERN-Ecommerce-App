41c17ea0076c0f21523ed6a09cfcca6c07cab873
pranav <prnvthir@gmail.com>
2021-04-20 23:45:48 -0400

    have cart state updating dynamically from store

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import { React, useEffect } from "react";
 import "./CartScreen.css";
+import { useSelector, useDispatch } from "react-redux";
+import {Link} from "react-router-dom";
 
 //components
 import CartItem from "../components/CartItem";
 
 const CartScreen = () => {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+
+  const { cartItems } = cart;
+
+
   return (
     <div className="cartscreen">
       <div className="cartscreen__left">
         <h2>Shopping Cart</h2>
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
+          {cartItems.length ===  0 ? (
+
+            <div>
+              CART IS EMPTY
+               <Link to="/">
+               GO BACK TO SHOP
+               </Link>
+            </div>
+          ) : cartItems.map((item)=>{
+
+            <CartItem/>
+
+
+
+          })
+          
+        
+        
+        }
+
+
       </div>
       <div className="cartscreen__right">
         <div className="cartscreen__info">
